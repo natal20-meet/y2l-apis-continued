3745a782a4f390370c802ad1d7b749c710e5f873
myname21-meet <myname21@meet.mit.edu>
2019-12-19 11:43:46 -0500

    lab api continued

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
+import requests, json
 app = Flask(__name__)
+
+headers = {'Authorization': 'Key <Y2604ac05607f440caa4c0d0e1d8a635e>'}
+
+api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
+
 
 @app.route('/')
 def home():
@@ -7,7 +14,6 @@ def home():
 
 @app.route('/study_image', methods = ['POST'])
 def study_image():
-    
     image_url = request.form['url-input']
     # At this point you have the image_url value from the front end
     # Your job now is to send this information to the Clarifai API
@@ -15,8 +21,17 @@ def study_image():
     # example we covered in the slides! 
 
     # YOUR CODE HERE!
-    
-    return render_template('home.html', results="No results yet :(")
+    data ={"inputs": [
+      {
+        "data": {
+          "image": {
+            "url": "https://samples.clarifai.com/metro-north.jpg"
+          }
+        }
+      }
+    ]}
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+    return render_template('home.html', results= response)
 
 if __name__ == '__main__':
     app.run(debug=True)
